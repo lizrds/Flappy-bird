39abbe972e2c7c3058a9c8fdcfb5f08e3c815e63
DeimisM <deimis.mardosas@outlook.com>
2023-10-12 20:22:18 +0300

    Added sounds and score

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bird : MonoBehaviour
 {
     public float jumpSpeed;
     public float rotatePower;
+    public TextMeshPro scoreText;
+    public AudioClip scoreSound;
+    int score = 0;
+    AudioSource source;
+
+
     Rigidbody2D rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        source = gameObject.AddComponent<AudioSource>();
     }
 
     private void Update()
@@ -23,4 +32,24 @@ public class Bird : MonoBehaviour
 
         transform.eulerAngles = new Vector3(0, 0, rb.velocity.y * rotatePower);
     }
+
+    void Die()
+    {
+        var sceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Die();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        score += 1;
+        source.clip = scoreSound;
+        source.Play();
+        scoreText.text = score.ToString();
+    }
 }
+
